6e97d85909590963dcacfe3a6a8765d9d5e23991
Dedicated407 <ilyaboy@list.ru>
2021-09-19 18:35:51 +0300

    feat: connect app with database

--- app/src/main/java/com/dedicated407/favoriteLiterature/MainActivity.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.dedicated407.favoriteLiterature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dedicated407.favoriteLiterature.Presentation.Repository.Repository
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        Repository(application)
     }
 }
