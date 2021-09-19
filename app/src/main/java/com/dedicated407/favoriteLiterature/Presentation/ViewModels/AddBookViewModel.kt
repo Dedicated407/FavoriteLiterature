f931062137eddb3c68d21da584456a4e4e0f0079
Dedicated407 <ilyaboy@list.ru>
2021-09-19 18:31:17 +0300

    feat: update AddBookFragment, add list in view

--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/AddBookViewModel.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/AddBookViewModel.kt
@@ -1,4 +1,18 @@
 package com.dedicated407.favoriteLiterature.Presentation.ViewModels
 
-class AddBookViewModel {
+import androidx.lifecycle.ViewModel
+import com.dedicated407.favoriteLiterature.Domain.Model.Book
+import com.dedicated407.favoriteLiterature.Domain.Model.User
+import com.dedicated407.favoriteLiterature.Presentation.Room.Model.BookDTO
+import com.dedicated407.favoriteLiterature.Presentation.Repository.Repository
+
+class AddBookViewModel : ViewModel() {
+    fun addBook(name: String, author: User, description: String?) {
+
+        val book = BookDTO(Book(name, author))
+        if (description != null)
+            book.description = description
+
+        Repository.getRepository().addBook(book)
+    }
 }
