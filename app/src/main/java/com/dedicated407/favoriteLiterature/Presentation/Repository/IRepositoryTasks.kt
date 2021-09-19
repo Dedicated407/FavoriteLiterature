d0c08362c2962da93f9f898b47cd9a55acd88446
Dedicated407 <ilyaboy@list.ru>
2021-09-19 18:24:04 +0300

    chore: update IRepositoryTasks

--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Repository/IRepositoryTasks.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Repository/IRepositoryTasks.kt
@@ -2,10 +2,11 @@ package com.dedicated407.favoriteLiterature.Presentation.Repository
 
 import androidx.lifecycle.LiveData
 import com.dedicated407.favoriteLiterature.Domain.Model.Book
+import com.dedicated407.favoriteLiterature.Presentation.Room.Model.BookDTO
 
 
-interface RepositoryTasks {
+interface IRepositoryTasks {
     fun getAllBooks(): LiveData<List<Book>>
-    fun addBook(book: Book)
-    fun deleteBook(book: Book)
+    fun <T: Book> addBook(book: T)
+    fun <T: Book> deleteBook(book: T)
 }
