582ee7184c4e99c9df34e80cc74c50e1b7937b7c
Dedicated407 <ilyaboy@list.ru>
2021-09-19 18:21:58 +0300

    chore: add BookDAO

---
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Room/DAO/IBookDAO.kt
@@ -0,0 +1,23 @@
+package com.dedicated407.favoriteLiterature.Presentation.Room.DAO
+
+import androidx.lifecycle.LiveData
+
+import androidx.room.Delete
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.dedicated407.favoriteLiterature.Presentation.Room.Model.BookDTO
+
+@Dao
+interface IBookDAO {
+    @Insert
+    fun addBook(book: BookDTO)
+
+    @Delete
+    fun deleteBook(book: BookDTO)
+
+    @Query("SELECT * FROM book")
+    fun getAllBooks(): LiveData<List<BookDTO>>
+}
+
