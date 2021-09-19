d2163dcf46e69c2f6c29c7faf32bbf9867e78d4d
Dedicated407 <ilyaboy@list.ru>
2021-09-19 18:24:47 +0300

    fix: update MockBase

--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Repository/Mock/MockBase.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Repository/Mock/MockBase.kt
@@ -1,80 +1,72 @@
 package com.dedicated407.favoriteLiterature.Presentation.Repository.Mock
 
-import androidx.lifecycle.LiveData
 import com.dedicated407.favoriteLiterature.Domain.Model.Book
 import com.dedicated407.favoriteLiterature.Domain.Model.User
-import com.dedicated407.favoriteLiterature.Presentation.Repository.RepositoryTasks
+import com.dedicated407.favoriteLiterature.Presentation.Repository.IRepositoryTasks
 import androidx.lifecycle.MutableLiveData
 
 
-public class MockBase : RepositoryTasks {
-    private var data: LiveData<List<Book>>
+class MockBase : IRepositoryTasks {
+    private var data: MutableLiveData<List<Book>>
     private var list: ArrayList<Book> = arrayListOf()
 
     init {
         val book1 = Book(
-            1,
             "Kotlin",
             User(
-                1,
                 "Ilya",
                 "Tsypin"
             )
         )
-        book1.author.patronymic = "Pavlovich"
-        book1.author.email = "ilyaboy@list.ru"
-        book1.author.phone = "+7 (800) 555-35-35"
+        book1.author?.patronymic = "Pavlovich"
+        book1.author?.email = "ilyaboy@list.ru"
+        book1.author?.phone = "+7 (800) 555-35-35"
         list.add(book1)
         val book2 = Book(
-            2,
             "The Bronze Horseman",
             User(
-                2,
                 "Alex",
                 "Pushkin"
             )
         )
-        book2.author.patronymic = "Sergeevich"
+        book2.author?.patronymic = "Sergeevich"
         list.add(book2)
         val book3 = Book(
-            3,
             "War and Peace",
             User(
-                3,
                 "Lev",
                 "Tolstoy"
             )
         )
-        book3.author.patronymic = "Nikolaevich"
+        book3.author?.patronymic = "Nikolaevich"
         list.add(book3)
         val book4 = Book(
-            4,
             "Ward №6",
             User(
-                4,
                 "Anton",
                 "Chekhov"
             )
         )
-        book4.author.patronymic = "Pavlovich"
+        book4.author?.patronymic = "Pavlovich"
         book4.description = "Ward №6. 1892. " +
                 "Summary of the story. " +
                 "It is read in 6 minutes, the original — 2 hours. " +
                 "In the county town, in a small hospital wing, " +
                 "there is ward №6 for the mentally ill."
         list.add(book4)
+
         data = MutableLiveData(list)
     }
 
-    override fun getAllBooks() = data
+    override fun getAllBooks(): MutableLiveData<List<Book>> = data
 
-    override fun addBook(book: Book) {
+    override fun <T : Book> addBook(book: T) {
         list.add(book)
 
         data = MutableLiveData(list)
     }
 
-    override fun deleteBook(book: Book) {
+    override fun <T : Book> deleteBook(book: T) {
         list.remove(book)
 
         data = MutableLiveData(list)
