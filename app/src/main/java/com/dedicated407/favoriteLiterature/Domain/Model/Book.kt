6f9448cacf4f775c19621c801a36ef50e38aebf3
Dedicated407 <ilyaboy@list.ru>
2021-09-19 18:19:32 +0300

    feat: update entities Book and User

--- app/src/main/java/com/dedicated407/favoriteLiterature/Domain/Model/Book.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Domain/Model/Book.kt
@@ -1,8 +1,5 @@
 package com.dedicated407.favoriteLiterature.Domain.Model
 
-import java.time.LocalDate
-
-public class Book(var id: Int, var name: String, var author: User) {
+open class Book(open val name: String? = null, open val author: User? = null) {
     var description: String? = null
-    var creationYear: LocalDate? = null
 }
