e323b191c3a0a16e97d3feeaecce98185b259e2f
Dedicated407 <ilyaboy@list.ru>
2021-09-28 13:19:02 +0300

    style: update book info view

--- app/src/main/java/com/dedicated407/favoriteLiterature/Domain/Model/User.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Domain/Model/User.kt
@@ -3,12 +3,12 @@ package com.dedicated407.favoriteLiterature.Domain.Model
 class User(
     var name: String,
     var lastName: String,
-    var patronymic: String? = null,
-    var phone: String? = null,
-    var email: String? = null,
+    var patronymic: String? = "",
+    var phone: String? = "",
+    var email: String? = "",
     open val images: List<String>? = null
 ) {
     override fun toString(): String {
-        return "$lastName $name"
+        return "$lastName $name $patronymic"
     }
 }
