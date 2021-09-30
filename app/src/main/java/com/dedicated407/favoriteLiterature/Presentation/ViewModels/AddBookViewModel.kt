ed765736666824be5bfe3991a59d739b104c9aed
Dedicated407 <ilyaboy@list.ru>
2021-10-01 00:19:57 +0300

    fix: the picture was not overwritten when the book was added

--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/AddBookViewModel.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/AddBookViewModel.kt
@@ -1,11 +1,22 @@
 package com.dedicated407.favoriteLiterature.Presentation.ViewModels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dedicated407.favoriteLiterature.Presentation.Room.Model.BookDTO
 import com.dedicated407.favoriteLiterature.Presentation.Repository.Repository
 
 class AddBookViewModel : ViewModel() {
+    private val mImage = MutableLiveData<String?>()
+    val image: LiveData<String?>
+        get() = mImage
+
     fun addBook(book: BookDTO) {
         Repository.getRepository().addBook(book)
     }
+
+    fun setImage(image: String?) {
+        mImage.value = image
+    }
+
 }
--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Views/AddBookFragment.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Views/AddBookFragment.kt
@@ -1,6 +1,8 @@
 package com.dedicated407.favoriteLiterature.Presentation.Views
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -28,14 +30,18 @@ class AddBookFragment : Fragment() {
         mBinding = AddBookFragmentBinding.inflate(layoutInflater, container, false)
 
         mBinding.btnUploadImage.setOnClickListener {
-            activity?.activityResultRegistry?.register("registerImage", OpenDocument()) { result ->
-                activity?.applicationContext?.contentResolver?.takePersistableUriPermission(
-                    result,
-                    Intent.FLAG_GRANT_READ_URI_PERMISSION
-                )
-                mBinding.bookImage.setImageURI(result)
-                images.add(images.size, result.toString())
-            }?.launch(arrayOf("image/*"))
+            requireActivity().activityResultRegistry.register("registerImage", OpenDocument())
+            { result ->
+                if (result != null) {
+                    requireActivity().applicationContext.contentResolver
+                        .takePersistableUriPermission(
+                            result,
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    )
+                    mViewModel.setImage(result.toString())
+                    images.add(images.size, mViewModel.image.value!!)
+                }
+            }.launch(arrayOf("image/*"))
         }
 
         mBinding.btnAddBook.setOnClickListener { v ->
@@ -53,7 +59,8 @@ class AddBookFragment : Fragment() {
                 Toast.makeText(context, "Added", Toast.LENGTH_SHORT).show()
 
                 mBinding.apply {
-                    bookImage.setImageURI(null)
+                    bookImage.visibility = View.GONE
+                    images.clear()
                     inputBookName.text = null
                     inputAuthorName.text = null
                     inputBookDescription.text = null
@@ -66,5 +73,25 @@ class AddBookFragment : Fragment() {
         mViewModel = ViewModelProvider(this)[AddBookViewModel::class.java]
 
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mViewModel = ViewModelProvider(this).get(AddBookViewModel::class.java)
+        mViewModel.image.observe(viewLifecycleOwner, {
+            if (it == null) {
+                mBinding.bookImage.setImageDrawable(null)
+                mBinding.bookImage.visibility = View.GONE
+            } else {
+                val uri = Uri.parse(mViewModel.image.value)
+                mBinding.bookImage.setImageBitmap(
+                    BitmapFactory.decodeFileDescriptor(
+                        requireContext().contentResolver.openFileDescriptor(
+                            uri, "r")?.fileDescriptor
+                    )
+                )
+                mBinding.bookImage.visibility = View.VISIBLE
+            }
+        })
     }
 }
