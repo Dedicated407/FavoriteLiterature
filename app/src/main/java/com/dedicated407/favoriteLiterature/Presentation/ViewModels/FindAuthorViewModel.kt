2c166e69270953c84ad36291862525595a2959da
Dedicated407 <ilyaboy@list.ru>
2021-10-14 13:01:07 +0300

    feat: add find author fragment

---
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/FindAuthorViewModel.kt
@@ -0,0 +1,7 @@
+package com.dedicated407.favoriteLiterature.Presentation.ViewModels
+
+import androidx.lifecycle.ViewModel
+
+class FindAuthorViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Views/FindAuthorFragment.kt
@@ -0,0 +1,7 @@
+package com.dedicated407.favoriteLiterature.Presentation.Views
+
+import androidx.fragment.app.Fragment
+
+class FindAuthorFragment: Fragment() {
+
+}
