97b1684fdb304d38fd3c497bbc392365f8a94094
Dedicated407 <ilyaboy@list.ru>
2021-10-17 14:51:06 +0300

    feat: update FindWriterFragment + add adapter for list of writers

--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/FindWriterViewModel.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/ViewModels/FindWriterViewModel.kt
@@ -1,7 +1,24 @@
 package com.dedicated407.favoriteLiterature.Presentation.ViewModels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
+import com.dedicated407.favoriteLiterature.DI.ServiceLocator
+import com.dedicated407.favoriteLiterature.Domain.Model.Writer
 
-class FindAuthorViewModel: ViewModel() {
+class FindWriterViewModel: ViewModel() {
+    private var mediatorLiveData = MediatorLiveData<List<Writer>>()
 
+    fun setWritersList(writerName: String) {
+        val serviceLocator = ServiceLocator()
+        mediatorLiveData.addSource(
+            serviceLocator.getInstance().getAnalysis().getWriters(writerName)
+        ) {
+            mediatorLiveData.value = it
+        }
+    }
+
+    fun getWritersList(): LiveData<List<Writer>> {
+        return mediatorLiveData
+    }
 }
--- app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Views/FindWriterFragment.kt
+++ app/src/main/java/com/dedicated407/favoriteLiterature/Presentation/Views/FindWriterFragment.kt
@@ -1,7 +1,47 @@
 package com.dedicated407.favoriteLiterature.Presentation.Views
 
+import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dedicated407.favoriteLiterature.Domain.Model.Writer
+import com.dedicated407.favoriteLiterature.Presentation.ViewModels.FindWriterViewModel
+import com.dedicated407.favoriteLiterature.Presentation.Views.Adapters.WritersListAdapter
+import com.dedicated407.favoriteLiterature.databinding.FindWriterFragmentBinding
 
-class FindAuthorFragment: Fragment() {
+class FindWriterFragment: Fragment() {
+    private lateinit var mViewModel: FindWriterViewModel
+    private lateinit var mBinding: FindWriterFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        mBinding = FindWriterFragmentBinding.inflate(layoutInflater, container, false)
+        mBinding.FindWriterList.RecyclerViewList.layoutManager = LinearLayoutManager(context)
+
+        mBinding.findWriterSearchView.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    mViewModel.setWritersList(query)
+                    return true
+                }
+            }
+        )
+
+        mViewModel = ViewModelProvider(this).get(FindWriterViewModel::class.java)
+
+        mViewModel.getWritersList().observe(viewLifecycleOwner) { writersList: List<Writer> ->
+            mBinding.FindWriterList.RecyclerViewList.adapter = WritersListAdapter(writersList)
+        }
+        return mBinding.root
+    }
 
 }
